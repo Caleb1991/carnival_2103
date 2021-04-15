3d199b9a5a642405baa84e9ba29762e8e62a8dcb
Caleb Wittman <calebjwittman1991@gmail.com>
2021-04-15 09:23:12 -0600

    Create test for name

--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -11,6 +11,12 @@ RSpec.describe Carnival do
       expect(jeffco_fair).to be_an_instance_of(Carnival)
     end
 
+    it 'has a name' do
+      jeffco_fair = Carnival.new('Jefferson County Fair')
+
+      expect(jeffco_fair.name).to eq('Jefferson County Fair')
+    end
+
     it 'has no rides by default' do
       jeffco_fair = Carnival.new('Jefferson County Fair')
 
