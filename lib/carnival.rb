343131ae22d4b2de2aa99c726065f6b0688d98ce
Caleb Wittman <calebjwittman1991@gmail.com>
2021-04-15 09:42:18 -0600

    Create recommend rides method

--- lib/carnival.rb
+++ lib/carnival.rb
@@ -10,4 +10,14 @@ class Carnival
   def add_ride(ride)
     @rides << ride
   end
+
+  def recommend_rides(attendee)
+    attendee_interests = attendee.interests
+
+    reccomendations = attendee_interests.map do |interest|
+      @rides.find_all do |ride|
+        interest == ride.name
+      end
+    end.flatten
+  end
 end
