5f2ea8f1bb8fc84928318a6b3f3ba402d770812f
Caleb Wittman <calebjwittman1991@gmail.com>
2021-04-15 09:11:07 -0600

    Create test to see it has spending money

--- spec/attendee_spec.rb
+++ spec/attendee_spec.rb
@@ -15,5 +15,11 @@ RSpec.describe Attendee do
 
       expect(attendee.name).to eq('Bob')
     end
+
+    it 'has spending money' do
+      attendee = Attendee.new('Bob', 20)
+
+      expect(attendee.spending_money).to eq(20)
+    end
   end
 end
